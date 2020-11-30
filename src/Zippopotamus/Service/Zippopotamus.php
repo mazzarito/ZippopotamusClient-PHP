20d57b1b70a25a565ddcfb50739129fc783e27f6
Matt <matt@blankstyle.com>
2020-11-30 05:14:01 -0800

    Php style updates (#2)
    * Fix code style, comments

--- src/Zippopotamus/Service/Zippopotamus.php
+++ src/Zippopotamus/Service/Zippopotamus.php
@@ -4,72 +4,91 @@
  *
  * @version 1.0.1
  */
-
 namespace Zippopotamus\Service;
 
 use Laminas\Http\Client;
 use Laminas\Http\Headers;
 use Laminas\Json\Json;
 
-final class Zippopotamus
-{
-    public static $apiUrl = 'http://api.zippopotam.us/';
+/**
+ * Class Zippopotamus
+ *
+ * @package Zippopotamus\Service
+ */
+final class Zippopotamus {
+  /**
+   * @var string $apiUrl
+   *   Static base aSPIl
+   */
+  public static $apiUrl = 'http://api.zippopotam.us/';
 
-    static public function code($countryCode, $postalCode)
-    {
-        $http = new Client();
+  /**
+   * @param $countryCode
+   * @param $postalCode
+   *
+   * @return mixed
+   */
+  static public function code($countryCode, $postalCode) {
+    $http = new Client();
+    $http->setOptions(array('sslverifypeer' => FALSE));
 
-        $http->setOptions(array('sslverifypeer' => false));
-        $headers = new Headers();
-        $headers->addHeaderLine('Content-Type', 'application/json');
-        $http->setHeaders($headers);
+    $headers = new Headers();
+    $headers->addHeaderLine('Content-Type', 'application/json');
 
-        $http->setUri(self::$apiUrl . urlencode($countryCode) . '/' . urlencode($postalCode));
-        $http->setMethod('GET');
+    $http->setHeaders($headers);
+    $http->setUri(self::$apiUrl . urlencode($countryCode) . '/' . urlencode($postalCode));
+    $http->setMethod('GET');
 
-        $response = $http->send();
-        $json     = Json::decode($response->getBody());
+    $response = $http->send();
+    $json = Json::decode($response->getBody());
 
-        return $json;
-    }
+    return $json;
+  }
 
-    static public function place($countryCode, $stateCode, $city)
-    {
-        $http = new Client();
+  /**
+   * @param $countryCode
+   * @param $stateCode
+   * @param $city
+   *
+   * @return mixed
+   */
+  static public function place($countryCode, $stateCode, $city) {
+    $http = new Client();
+    $http->setOptions(array('sslverifypeer' => FALSE));
 
-        $http->setOptions(array('sslverifypeer' => false));
-        $headers = new Headers();
-        $headers->addHeaderLine('Content-Type', 'application/json');
-        $http->setHeaders($headers);
+    $headers = new Headers();
+    $headers->addHeaderLine('Content-Type', 'application/json');
 
-        $http->setUri(self::$apiUrl
-            . urlencode($countryCode) . '/'
-            . urlencode($stateCode) . '/'
-            . urlencode($city)
-        );
-        $http->setMethod('GET');
+    $http->setHeaders($headers);
+    $http->setUri(self::$apiUrl . urlencode($countryCode) . '/' . urlencode($stateCode) . '/' . urlencode($city));
+    $http->setMethod('GET');
 
-        $response = $http->send();
-        $json     = Json::decode($response->getBody());
+    $response = $http->send();
+    $json = Json::decode($response->getBody());
 
-        return $json;
-    }
+    return $json;
+  }
 
-    static public function nearby($countryCode, $postalCode)
-    {
-        $http = new Client();
+  /**
+   * @param $countryCode
+   * @param $postalCode
+   *
+   * @return mixed
+   */
+  static public function nearby($countryCode, $postalCode) {
+    $http = new Client();
+    $http->setOptions(array('sslverifypeer' => FALSE));
 
-        $http->setOptions(array('sslverifypeer' => false));
-        $headers = new Headers();
-        $headers->addHeaderLine('Content-Type', 'application/json');
-        $http->setHeaders($headers);
+    $headers = new Headers();
+    $headers->addHeaderLine('Content-Type', 'application/json');
 
-        $http->setUri(self::$apiUrl . 'nearby/' . urlencode($countryCode) . '/' . urlencode($postalCode));
-        $http->setMethod('GET');
+    $http->setHeaders($headers);
+    $http->setUri(self::$apiUrl . 'nearby/' . urlencode($countryCode) . '/' . urlencode($postalCode));
+    $http->setMethod('GET');
 
-        $response = $http->send();
-        $json     = Json::decode($response->getBody());
+    $response = $http->send();
+    $json = Json::decode($response->getBody());
 
-        return $json;
-    }
+    return $json;
+  }
 }
